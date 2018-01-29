8e6eecd9697d8720891d25fc95ea56504ed6c93e
Zac <megaflop10@hotmail.com>
2018-01-29 11:37:12 +1100

    Added Teleop Drive Code
    Standard Arcade drive, with 30%, 60%, 80% presets, and reversing.

--- src/au/net/projectb/subsystems/Drivetrain.java
+++ src/au/net/projectb/subsystems/Drivetrain.java
@@ -15,6 +15,16 @@ public class Drivetrain extends Subsystem {
 	TalonSRX mLeftMaster, mLeftSlaveA, mLeftSlaveB;
 	TalonSRX mRightMaster, mRightSlaveA, mRightSlaveB;
 	
+	boolean driveDirectionIsForwards;
+	double throttlePreset;
+	
+	enum ThrottlePreset {
+		ANALOGUE,
+		LOW,
+		MID,
+		HIGH
+	}
+	
 	public static Drivetrain getInstance() {
 		if (instance == null) {
 			instance = new Drivetrain();
@@ -60,5 +70,59 @@ public class Drivetrain extends Subsystem {
 		
 		mRightSlaveA.set(ControlMode.Follower, 0);
 		mRightSlaveB.set(ControlMode.Follower, 0);
+		
+		driveDirectionIsForwards = true;
+		throttlePreset = -1.0;
+	}
+	
+	/**
+	 * Arcade drive, sets motor powers.
+	 * @param power
+	 * @param steering
+	 * @param throttle
+	 */
+	public void arcadeDrive(double power, double steering, double throttle) {
+		if (throttlePreset != -1.0) {
+			throttle = throttlePreset;
+		}
+		if (!driveDirectionIsForwards) {
+			throttle = -throttle;
+		}
+		double leftPower = (power + steering) * throttle;
+		double rightPower = (power - steering) * throttle;
+		setMotorPower(leftPower, rightPower);
+	}
+	
+	/**
+	 * Reverse the driving direction for teleopDrive.
+	 */
+	public void reverseDirection() {
+		driveDirectionIsForwards = !driveDirectionIsForwards;
+	}
+	
+	/**
+	 * Wrapper for setting motor powers. Only Drivetrain can do so directly.
+	 * @param left
+	 * @param right
+	 */
+	private void setMotorPower(double left, double right) {
+		mLeftMaster.set(ControlMode.PercentOutput, left);
+		mRightMaster.set(ControlMode.PercentOutput, -right);
+	}
+	
+	public void setThrottlePreset(ThrottlePreset preset) {
+		switch (preset) {
+			case LOW:
+				throttlePreset = 0.3;
+				break;
+			case MID:
+				throttlePreset = 0.6;
+				break;
+			case HIGH:
+				throttlePreset = 0.8;
+				break;
+			default: // Also handles ANALOGUE
+				throttlePreset = -1.0;
+		}
 	}
 }
